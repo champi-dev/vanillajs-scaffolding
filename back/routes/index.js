765d33c56c7e9111b8f8d411307b4a359d93c432
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2020-06-16 07:29:46 -0500

    Add /create and /remove routes

--- back/routes/index.js
+++ back/routes/index.js
@@ -19,4 +19,40 @@ router.post('/update', function (req, res, next) {
   res.status(200).json(found);
 });
 
+router.post('/create', function (req, res, next) {
+  const found = db.get('artists')
+    .find({ name: req.body.name })
+    .value()
+
+  if (found) return res.status(401).send()
+
+  const artistsLength = db.get('artists')
+  .size()
+  .value()
+
+  db.set(`artists[${artistsLength}]`, req.body)
+  .write()
+
+  res.status(200).send()
+})
+
+router.post('/remove', function (req, res, next) {
+  try {
+    const artists = db.get('artists').value()
+    const filtered = [] 
+  
+    Object.values(artists).forEach(function (a) {
+      if (a.name !== req.body.name) filtered.push(a)
+    })
+  
+    db.set('artists', filtered)
+    .write()
+  
+    res.status(200).send()
+  } catch (e) {
+    console.log(e)
+    res.status(404).send()
+  }
+})
+
 module.exports = router;
